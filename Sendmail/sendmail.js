8ec7787630f71a1c517b0072defa68e3e6f585ff
L009 <danmzzu@gmail.com>
2025-05-23 06:41:20 -0300

    Upload

--- Sendmail/sendmail.js
+++ Sendmail/sendmail.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const DEFAULT_EMAIL_USER = 'contato@l009.com.br';
 const DEFAULT_EMAIL_PASS = process.env.EMAIL_PASS;
